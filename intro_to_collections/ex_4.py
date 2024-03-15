df6395118400f098a1c0c4aeeca1111c8a54ee04
AngelQubits <156223103+AngelQubits@users.noreply.github.com>
2024-03-15 09:11:55 +0000

    added intro_to_collections exerces 1 - 5

---
+++ intro_to_collections/ex_4.py
@@ -0,0 +1,2 @@
+# Strings = sequence because they can be indexed, as each
+# character can be treated as a separate string and referenced as such.
---
+++ intro_to_collections/ex_5.py
@@ -0,0 +1,3 @@
+# Set Type differ from sequences:
+# 1: sequences are ordered and can be indexed, sets are not
+# order does not matter with sets.
